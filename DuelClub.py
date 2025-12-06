a1da2a94bc72dfbb8e1bff02757c6e3a602fceae
Siddhart <Siddharth.pandey1@gmail.com>
2025-12-06 20:46:47 +0530

    Workdone

---
+++ DuelClub.py
@@ -0,0 +1,6 @@
+print("Let's welcome Harry potter and Dhruv to the Duel Club!")
+print("Cast your spells wisely and may the best wizard win!")
+spells = ["Expelliarmus", "Stupefy", "Protego", "Rictusempra", "Lumos", "Alohomora", "Expecto Patronum"]
+for spell in spells:
+    print(f"Dhruv casts {spell}!")
+    print(f"Harry counters with {spell}!")
